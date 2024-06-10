524f9b2b2772159184c303abc93d4010acaca526
guru-code-hub <avgururaj82@gmail.com>
2024-06-10 18:51:48 +0530

    Adding latest changes

---
+++ Py_Exercise/Assignment001.py
@@ -0,0 +1,7 @@
+# Develop a Python script that calculates the square and cube of a given number.
+# num = 2 sq = 4, c = 8
+
+sqr_num = pow(2, 2)
+cube_num = pow(2, 3)
+print("The Square of 2 is - ", sqr_num)
+print("The Cube of 2 is - ", cube_num)
---
+++ Py_Exercise/Assignment002.py
@@ -0,0 +1,12 @@
+# Create a program that takes two numbers as input
+# and prints whether the first number is greater than,
+# less than, or equal to the second number.
+
+first_num = input("Enter first number :")
+second_num = input("Enter second number :")
+if first_num > second_num:
+    print("First number is greater than Second number")
+elif first_num == second_num:
+    print("First number and Second number are equal")
+else:
+    print("Second number is greater than First number")
